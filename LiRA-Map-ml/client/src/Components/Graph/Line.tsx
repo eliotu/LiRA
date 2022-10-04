26204ea9a0c6411c239cfcf0de6645e53d672f87
eliotu <eliot.ullmo@epfl.ch>
2022-10-04 10:54:56 +0200

    filter basic

--- LiRA-Map-ml/client/src/Components/Graph/Line.tsx
+++ LiRA-Map-ml/client/src/Components/Graph/Line.tsx
@@ -38,7 +38,9 @@ const Line: FC<ILine> = ( { svg, xAxis, yAxis, data, bounds, label, i, time } ) 
 
         const onHover = (d: DotHover | undefined) => d === undefined 
             ? setDotHover( undefined )
-            : setDotHover( { ...d, x: d.x / _bounds.maxX } )
+            : 
+            console.log("being hover for real");
+            setDotHover( { ...d, x: d.x / _bounds.maxX } )
 
         const line = new GLine(svg, label, i, data, xAxis, yAxis, onHover, time)
 
